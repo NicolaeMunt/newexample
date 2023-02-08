96ac908dc7b756f323efe2682013c9b5f7df0da5
NicolaeMunt <114097090+NicolaeMunt@users.noreply.github.com>
2023-02-08 20:40:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>iMAGE Upload Using PHP</title>
+    <style>
+        body {
+            display:flex;
+            justify-content:center;
+            align-items: center;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
